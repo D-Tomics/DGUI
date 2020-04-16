559c409c1a00b6e2ee688f0b464c498b0b414ae9
D-Tomics <akm001gaming@gmail.com>
2020-04-16 23:01:10 +0530

    changed style of D_List

--- src/engine/ui/gui/components/D_List.java
+++ src/engine/ui/gui/components/D_List.java
@@ -1,9 +1,15 @@
 package engine.ui.gui.components;
 
+import engine.ui.gui.manager.events.D_GuiFocusLooseEvent;
+import engine.ui.gui.manager.events.D_GuiKeyPressEvent;
 import engine.ui.gui.manager.events.D_GuiMousePressEvent;
+import engine.ui.utils.D_Event;
+import engine.ui.utils.abstractions.Listener;
 import engine.ui.utils.observers.Observable;
 
 import java.util.HashMap;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 public class D_List<T> extends D_Component{
 
@@ -12,9 +18,21 @@ public class D_List<T> extends D_Component{
 
     private HashMap<String, T> items;
 
+    private D_GuiQuad container;
     private D_GuiQuad selected;
+
+    private final Listener ON_CELL_SELECT = e -> {
+        selected.setHoverable(true);
+        selected = (D_GuiQuad) e.getSource();
+        this.setSelected(false);
+    };
+
     public D_List(T...items) {
         this.style.setBounds(0,0,CELL_WIDTH,CELL_HEIGHT);
+        this.container = new D_GuiQuad(CELL_WIDTH,CELL_HEIGHT * items.length);
+        this.container.setVisible(false);
+        this.container.setHoverable(false);
+        this.addQuad(container);
         this.setSelectable(true);
         addItem(items);
     }
@@ -33,27 +51,26 @@ public class D_List<T> extends D_Component{
         if(items.containsKey(item.toString())) return;
         items.put(item.toString(),item);
 
-        D_GuiQuad quad = new D_GuiQuad(CELL_WIDTH, CELL_HEIGHT, item.toString());
+        D_GuiQuad quad = new D_GuiQuad(CELL_WIDTH - 2, CELL_HEIGHT - 2, item.toString());
+        quad.addEventListener(D_GuiMousePressEvent.class, ON_CELL_SELECT);
+        quad.style.setBorderSize(0);
         addQuad(quad);
-        quad.addEventListener(D_GuiMousePressEvent.class, e -> {
-            selected.setHoverable(true);
-            selected = (D_GuiQuad) e.getSource();
-            this.setSelected(false);
-        });
 
         if(selected == null) {
             selected = quad;
             selected.setVisible(true);
-            selected.style.setPosition(style.getX(),style.getY());
+            selected.style.setBorderSize(1);
+            selected.style.setCenter(style.getCenter());
         }
     }
 
     @Override
     public void onStateChange(Observable o) {
         if(this.getQuads() != null) {
-            float y = style.getY() - CELL_HEIGHT;
+            float y = style.getY() - 3 * CELL_HEIGHT / 2.0f;
             for(D_GuiQuad cell : getQuads()) {
-                cell.style.setPosition(this.style.getX(), y);
+                if(cell == container) continue;
+                cell.style.setCenter(this.style.getCenterX(), y);
                 y -= CELL_HEIGHT;
                 cell.setVisible(this.isSelected());
             }
@@ -62,15 +79,28 @@ public class D_List<T> extends D_Component{
         if(selected != null) {
             if(!this.isSelected()) {
                 selected.setVisible(true);
-                selected.style.setPosition(style.getX(),style.getY());
+                selected.style.setCenter(style.getCenter());
                 selected.setHoverable(false);
-            } else
+                container.setVisible(false);
+            } else {
+                container.style.setPosition(style.getX(), style.getY() - CELL_HEIGHT);
+                container.setVisible(true);
                 selected.setHoverable(true);
+            }
         }
     }
 
     @Override
     protected void onUpdate() {
+        if(getQuads() != null) {
+            for(D_GuiQuad quad : getQuads()) {
+                if(quad == container) continue;
+                if(quad.isHovered())
+                    quad.style.setBorderSize(1);
+                else
+                    quad.style.setBorderSize(0);
+            }
+        }
     }
 
 }
