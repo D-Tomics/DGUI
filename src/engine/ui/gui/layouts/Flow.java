abc012d870fffd6344f9383019227329c84271c6
D-Tomics <akm001gaming@gmail.com>
2020-04-27 13:46:24 +0530

    fixed flow layout

--- src/engine/ui/gui/layouts/Flow.java
+++ src/engine/ui/gui/layouts/Flow.java
@@ -2,72 +2,67 @@ package engine.ui.gui.layouts;
 
 import engine.ui.gui.components.D_Container;
 import engine.ui.gui.components.D_Gui;
-import engine.ui.gui.manager.Style;
 import engine.ui.gui.manager.constraints.D_LayoutConstraint;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Flow extends Layout {
 
+    private ArrayList<D_Gui> currentRow = new ArrayList<>();
+
     @Override
     public void update(D_Container parent) {
+        if(parent == null) return;
         if(!parent.isVisible()) return;
-        Style style = parent.getStyle();
-        List<D_Gui> childList = parent.getChildList();
-        if(childList == null) return;
+        if(parent.getChildList() == null) return;
 
-        float curX = style.getX() + style.getMarginLeft();
-        float curY = style.getY() - style.getMarginTop();
+        float x = parent.getStyle().getX() + parent.getStyle().getPaddingLeft();
+        float y = parent.getStyle().getY() - parent.getStyle().getPaddingTop();
 
-        for(D_Gui gui : childList) {
-            if(gui.isVisible()) continue;
-            gui.getStyle().setPosition(curX, curY);
-
-            curX += gui.getStyle().getWidth() + gui.getStyle().getMarginLeft();
-        }
-        parent.getStyle().setWidth(curX -parent.getStyle().getX(), false);
-
-        /*if(!parent.isVisible()) return;
-
-        Style style = parent.getStyle();
-        var pNode = parent.getChildList();
-        if(pNode == null) return;
-
-        float currentX = style.getX() + getPaddingLeft() + getMarginLeft();
-        float currentY = style.getY() - (getPaddingTop() + getMarginTop());
-        float width = 0;
-        float height = 0;
-
-        float prevMaxRowHeight = 0;
-        for(var gui : pNode) {
-            if(!gui.isVisible()) continue;
-            var cStyle = gui.getStyle();
-
-            cStyle.setX(currentX);
-            cStyle.setY(currentY);
-
-            if(currentX + cStyle.getWidth() + getPaddingRight() >= style.getX() + style.getWidth()) {
-                currentY -= prevMaxRowHeight + getPaddingTop();
-                width = currentX + cStyle.getWidth() + getPaddingRight() - style.getX();
-                height = style.getY() - (currentY - cStyle.getHeight() - getPaddingBottom());
-                currentX = style.getX() + getPaddingRight() + getMarginLeft();
-                prevMaxRowHeight = cStyle.getHeight();
-            } else {
-                currentX += cStyle.getWidth() + getPaddingLeft();
+        currentRow.clear();
+        for(D_Gui child : parent.getChildList()){
+            if(x + child.getStyle().getWidth() + child.getStyle().getMarginWidth() + parent.getStyle().getPaddingRight() > parent.getStyle().getX() + parent.getStyle().getWidth()) {
+                positionComponents(parent,currentRow,y);
+                x = parent.getStyle().getX() + parent.getStyle().getPaddingLeft();
+                y -=  getRowHeight(currentRow);
+                currentRow.clear();
             }
-
-            if(cStyle.getHeight() > prevMaxRowHeight)
-                prevMaxRowHeight = cStyle.getHeight();
-
+            currentRow.add(child);
+            x += child.getStyle().getWidth() + child.getStyle().getMarginWidth();
         }
 
-        style.setWidth(width);
-        style.setHeight(height);*/
+        positionComponents(parent,currentRow,y);
     }
+
+    private void positionComponents(D_Container parent,ArrayList<D_Gui> row, float y) {
+        float w = getRowWidth(row);
+        float h = getRowHeight(row);
+
+        float x = parent.getStyle().getCenterX() + parent.getStyle().getPaddingLeft() - w / 2.0f;
+        for(D_Gui gui : row) {
+            gui.getStyle().setPosition(x + gui.getStyle().getMarginLeft(),y - h / 2.0f + gui.getStyle().getHeight() / 2.0f );
+            x += gui.getStyle().getWidth() + gui.getStyle().getMarginWidth();
+        }
+    }
+
+    private float getRowHeight(ArrayList<D_Gui> row) {
+        float max = 0;
+        for(D_Gui gui : row)
+            if(max < gui.getStyle().getHeight() + gui.getStyle().getMarginHeight())
+                max = gui.getStyle().getHeight() + gui.getStyle().getMarginHeight();
+        return max;
+    }
+
+    private float getRowWidth(ArrayList<D_Gui> row) {
+        float w = 0;
+        for(D_Gui gui : row)
+            w += gui.getStyle().getWidth() + gui.getStyle().getMarginWidth();
+        return w;
+    }
+
+
 
     @Override
-    void setConstraint(D_Gui gui, D_LayoutConstraint constraint) {
-        
-    }
+    void setConstraint(D_Gui gui, D_LayoutConstraint constraint) { }
 
 }
