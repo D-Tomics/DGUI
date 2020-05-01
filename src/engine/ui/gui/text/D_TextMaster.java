ac17fa5424c84ed83888edb2d2d34906a6c2757d
D-Tomics <akm001gaming@gmail.com>
2020-05-01 21:11:30 +0530

    Texts are now stored per Window instead of per Application

--- src/engine/ui/gui/text/D_TextMaster.java
+++ src/engine/ui/gui/text/D_TextMaster.java
@@ -4,58 +4,52 @@ import engine.ui.IO.Window;
 import engine.ui.IO.events.GLFWEvent;
 import engine.ui.IO.events.GLFWListener;
 import engine.ui.IO.events.GLFWWindowSizeEvent;
-import engine.ui.gui.renderer.TextRenderer;
 import engine.ui.gui.text.font.Font;
 
 import java.util.*;
 
 public final class D_TextMaster {
 
-    private static boolean initialized = false;
-    private static Map<Font, List<D_TextBox>> textMap;
+    private static Map<Window,Map<Font, List<D_TextBox>>> textMap;
 
     private static GLFWListener sizeChangeListener = new GLFWListener(GLFWWindowSizeEvent.class) {
         @Override
         public void invoke(GLFWEvent event) {
             if(textMap == null) return;
-            Set<Font> fonts = textMap.keySet();
+            Map<Font, List<D_TextBox>> map = textMap.get(event.getSource());
+            if(map == null) return;
+            Set<Font> fonts = map.keySet();
             for(Font font : fonts) {
-                List<D_TextBox> texts = textMap.get(font);
-                for(D_TextBox text : texts)
-                    text.getMesh().updateData(text);
+                List<D_TextBox> texts = map.get(font);
+                for(D_TextBox text : texts) {
+                    text.requestUpdate();
+                    text.update(event.getSource().getLoader());
+                }
             }
         }
     };
 
-    public static void init() {
-        if(initialized) return;
-        Window.INSTANCE.addListener(sizeChangeListener);
+    public static void init(Window window) {
+        window.addListener(sizeChangeListener);
     }
 
     protected static void load(D_TextBox text) {
-        if(textMap == null) textMap = new HashMap<>();
-        List<D_TextBox> texts = textMap.get(text.getFont());
-        if(texts == null) {
-            texts = new ArrayList<>();
-            textMap.put(text.getFont(),texts);
-        }
+        if(textMap == null)
+            textMap = new HashMap<>();
+        Map<Font, List<D_TextBox>> map = textMap.computeIfAbsent(Window.INSTANCE, k -> new HashMap<>());
+        List<D_TextBox> texts = map.computeIfAbsent(text.getFont(),k -> new ArrayList<>());
         texts.add(text);
     }
 
+    public static  Map<Font, List<D_TextBox>> getTextMap(Window window) { return textMap != null ? textMap.get(window) : null; }
+
     protected static void remove(D_TextBox text) {
         if(textMap == null || text == null) return;
-        List<D_TextBox> texts = textMap.get(text.getFont());
-        if(texts == null) return;
-        texts.remove(text);
+        for (Map<Font, List<D_TextBox>> maps : textMap.values()) {
+            List<D_TextBox> texts = maps.get(text.getFont());
+            if(texts == null) continue;
+            if(!texts.contains(text)) continue;
+            texts.remove(text);
+        }
     }
-
-    public static void render() {
-        if(textMap == null) return;
-        TextRenderer.render(textMap);
-    }
-
-    public static void cleanUp() {
-        TextRenderer.cleanUp();
-    }
-
 }
