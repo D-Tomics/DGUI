31c57da7bd1c4df2c6d555978245429a45b223e8
D-Tomics <akm001gaming@gmail.com>
2020-05-04 02:00:36 +0530

    relative position now takes a window instance to which the gui is relatively positioned

--- src/engine/ui/gui/manager/constraints/gui_constraints/RelativePosition.java
+++ src/engine/ui/gui/manager/constraints/gui_constraints/RelativePosition.java
@@ -8,19 +8,26 @@ public class RelativePosition extends D_Constraint {
 
     private float relativeX;
     private float relativeY;
+    private Window window;
 
-    public RelativePosition(String xPercent, String yPercent) {
+    public RelativePosition(Window window, String xPercent, String yPercent) {
         this(Float.parseFloat(xPercent) / 100.0f, Float.parseFloat(yPercent) / 100.0f);
     }
 
+    @Deprecated
     public RelativePosition(float relativeX, float relativeY) {
+        this(Window.INSTANCE,relativeX, relativeY);
+    }
+
+    public RelativePosition(Window window,float relativeX, float relativeY) {
+        this.window = window;
         this.relativeX = relativeX - 0.5f;
         this.relativeY = 0.5f - relativeY;
     }
 
     @Override
     public void update(D_Gui gui) {
-        gui.getStyle().setPosition(relativeX * Window.INSTANCE.getWidth(), relativeY * Window.INSTANCE.getHeight(), false);
+        gui.getStyle().setPosition(relativeX * window.getWidth(), relativeY * window.getHeight(), false);
     }
 
 }
