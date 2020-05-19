a01c198eb25a865af862022f91741041cb0f5838
D-Tomics <akm001gaming@gmail.com>
2020-05-19 14:56:53 +0530

    reverted grid layout back before the commit 514e738f0ac89a578cf0cc372dc52643e24a91cd

--- src/engine/ui/gui/layouts/GridLayout.java
+++ src/engine/ui/gui/layouts/GridLayout.java
@@ -14,8 +14,6 @@ public class GridLayout extends Layout {
     private boolean init;
     private int rows;
     private int columns;
-    private float defaultCellWidth;
-    private float defaultCellHeight;
     private float[] cellWidths;
     private float[] cellHeights;
     private GridConstraint defaultConstraint = new GridConstraint(0,0,0,0,Alignment.CENTER);
@@ -80,15 +78,9 @@ public class GridLayout extends Layout {
     }
 
     private void initCellSizes(D_Container parent) {
-        if(init) {
-            if(defaultCellWidth == parent.getStyle().getWidth() / columns &&
-                    defaultCellHeight == parent.getStyle().getHeight() / rows)
-                return;
-        }
-        defaultCellWidth = parent.getStyle().getWidth() / columns;
-        defaultCellHeight = parent.getStyle().getHeight() / rows;
-        Arrays.fill(cellWidths, defaultCellWidth);
-        Arrays.fill(cellHeights, defaultCellHeight);
+        if(init) return;
+        Arrays.fill(cellWidths, parent.getStyle().getWidth() / columns);
+        Arrays.fill(cellHeights, parent.getStyle().getHeight() / rows);
         init = true;
     }
 
