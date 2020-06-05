e0965886d589ad25937b949935ebc5416d9a051b
D-Tomics <akm001gaming@gmail.com>
2020-06-05 20:28:13 +0530

    refactored the code for Color class

--- src/engine/ui/utils/colors/Color.java
+++ src/engine/ui/utils/colors/Color.java
@@ -12,49 +12,41 @@ public final class Color {
 
     private float brightness = 1;
 
-    public Color() {
-        this(0);
-    }
-    public Color(float d) {
-        this(d,d,d);
-    }
-    public Color(float r, float g, float b) { this(r, g, b, 1); }
-    public Color(float r, float g, float b, float a) { set(r,g,b,a); }
-    public Color(Color color) { this.set(color); }
+    public Color()                                   { this(0);          }
+    public Color(float d)                            { this(d,d,d);         }
+    public Color(float r, float g, float b)          { this(r, g, b, 1); }
+    public Color(float r, float g, float b, float a) { this.set(r,g,b,a);   }
+    public Color(Color color)                        { this.set(color);     }
 
-    public Color set(float r, float g, float b, float a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
-        return this;
-    }
+    public Color set(float r, float g, float b)          { return this.set(r,g,b,a);        }
+    public Color set(float r, float g, float b, float a) { return this.r(r).g(g).b(b).a(a); }
 
-    public Color set(float r, float g, float b) { return this.set(r,g,b,1);}
     // integer colors are varied from 0 to 255
-    public Color set(int r, int g, int b) { return this.set(r / 255f, g / 255f, b / 255f, 1f);}
-    public Color set(int r, int g, int b, int a) { return this.set(r / 255f, g / 255f, b / 255f, a / 255f);}
+    public Color set(int r, int g, int b)        { return this.set(r / 255f, g / 255f, b / 255f, a);           }
+    public Color set(int r, int g, int b, int a) { return this.set(r / 255f, g / 255f, b / 255f, a / 255f); }
+
     public Color set(Color color) { return set(color.r(), color.g(), color.b(), color.a()); }
 
     public Color setBrightness(float brightness) { this.brightness = brightness; return this; }
     public float getBrightness() { return brightness; }
 
     //0xrrggbb
-    public void set(int color) {
+    public Color set(int color) {
         this.r = ((byte) ((color >> 16) & 0xFF)) / 255.0f;
         this.g = ((byte) ((color >>  8) & 0xFF)) / 255.0f;
         this.b = ((byte) ( color        & 0xFF)) / 255.0f;
+        return this;
     }
 
     public float r() { return r * brightness; }
     public float g() { return g * brightness; }
     public float b() { return b * brightness; }
-    public float a() { return a; }
+    public float a() { return a;              }
 
-    public void r(float r) { this.r = r; }
-    public void g(float g) { this.g = g; }
-    public void b(float b) { this.b = b; }
-    public void a(float a) { this.a = a; }
+    public Color r(float r) { this.r = r; return this; }
+    public Color g(float g) { this.g = g; return this; }
+    public Color b(float b) { this.b = b; return this; }
+    public Color a(float a) { this.a = a; return this; }
 
     public static Color mix(Color a, Color b, float mix) {
         return mix(a, b, new Color(), mix);
