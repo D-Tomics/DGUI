4bac9fc6a25af30e2cabba3c405676fdc289f076
D-Tomics <akm001gaming@gmail.com>
2020-09-15 02:28:11 +0530

    D_Canvas: added a method to add external textures

--- src/main/java/org/dtomics/DGUI/gui/components/D_Canvas.java
+++ src/main/java/org/dtomics/DGUI/gui/components/D_Canvas.java
@@ -1,13 +1,17 @@
-package engine.ui.gui.components;
+package org.dtomics.DGUI.gui.components;
 
-import engine.ui.gui.renderer.Texture;
-import engine.ui.utils.colors.Color;
+import org.dtomics.DGUI.gui.renderer.Texture;
+import org.dtomics.DGUI.utils.colors.Color;
 
 public class D_Canvas extends D_Panel {
 
     public D_Canvas(int width, int height) {
         this.style.setSize(width, height);
         this.style.setBgColor(Color.WHITE);
+    }
+
+    public void attachTexture(int texture, int width, int height) {
+        this.style.setBgTexture(new Texture(texture, width, height));
     }
 
     public void attachTexture(Texture texture) {
