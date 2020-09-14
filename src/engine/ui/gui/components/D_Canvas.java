739768613b12c39b9843962a3938a508d71380ec
D-Tomics <akm001gaming@gmail.com>
2020-09-15 02:26:36 +0530

    D_Canvas is now a panel type

--- src/engine/ui/gui/components/D_Canvas.java
+++ src/engine/ui/gui/components/D_Canvas.java
@@ -1,21 +1,17 @@
 package engine.ui.gui.components;
 
 import engine.ui.gui.renderer.Texture;
-import engine.ui.utils.observers.Observable;
+import engine.ui.utils.colors.Color;
 
-public class D_Canvas extends D_Gui{
+public class D_Canvas extends D_Panel {
 
     public D_Canvas(int width, int height) {
         this.style.setSize(width, height);
+        this.style.setBgColor(Color.WHITE);
     }
 
     public void attachTexture(Texture texture) {
         this.style.setBgTexture(texture);
     }
 
-    @Override
-    protected void onUpdate() { }
-
-    @Override
-    protected void onStateChange(Observable o) { }
 }
