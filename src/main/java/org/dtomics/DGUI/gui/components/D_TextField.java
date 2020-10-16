c6d8831aefec14ecdba853800767fca1ac34dd1c
D-Tomics <akm001gaming@gmail.com>
2020-10-17 01:51:17 +0530

    feat: text component now sized via width and height instead of rows and cols

--- src/main/java/org/dtomics/DGUI/gui/components/D_TextField.java
+++ src/main/java/org/dtomics/DGUI/gui/components/D_TextField.java
@@ -1,8 +1,10 @@
 package org.dtomics.DGUI.gui.components;
 
+import org.dtomics.DGUI.gui.manager.constraints.guiTextConstraints.D_TextAlignCenter;
 import org.dtomics.DGUI.gui.manager.constraints.guiTextConstraints.D_TextAlignLeft;
 import org.dtomics.DGUI.gui.manager.constraints.guiTextConstraints.D_TextAlignTop;
 import org.dtomics.DGUI.gui.manager.constraints.guiTextConstraints.D_TextFill;
+import org.dtomics.DGUI.gui.text.meshCreator.TextAlignment;
 import org.dtomics.DGUI.utils.observers.Observable;
 
 /**
@@ -12,9 +14,10 @@ import org.dtomics.DGUI.utils.observers.Observable;
  */
 public class D_TextField extends D_TextComponent {
 
-    public D_TextField(String text, int cols) {
-        super(text, 1, cols);
-        this.addConstraint(new D_TextAlignTop(this.textBox, 0, new D_TextAlignLeft(this.textBox, 5, new D_TextFill(this.textBox, 10, 0))));
+    public D_TextField(String text, int width) {
+        super(text, width, 25);
+        this.textBox.setTextAlignment(TextAlignment.LEFT);
+        this.addConstraint(new D_TextAlignCenter(this.textBox, new D_TextAlignLeft(this.textBox, 5, new D_TextFill(this.textBox, 10, 0))));
     }
 
     @Override
